ea67282791126fd92733f37ab3b4f4fa660d97a5
Johannes Kliemann <jk@jkliemann.de>
2017-10-22 20:52:50 +0200

    degub mount

--- init.c
+++ init.c
@@ -1,16 +1,75 @@
 #include <unistd.h>
 #include <sys/mount.h>
 #include <sys/stat.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <dirent.h>
+
+void ls(const char *d)
+{
+    printf("ls %s -> ", d);
+    DIR *here = opendir(d);
+    struct dirent *entry;
+    while((entry = readdir(here))){
+        printf("%s ", entry->d_name);
+    }
+    printf("\n");
+}
+
+void create_and_mount(const char *dev)
+{
+    printf("%s %s\n", __func__, dev);
+    if(mknod (dev, S_IFBLK|0644, 8 << 8 | 5))
+        perror("mknod");
+    if(mount (dev, "/root", "fat32", MS_RDONLY, NULL))
+        perror("mount");
+}
 
 int
 main (int argc __attribute__((unused)), char *argv[] __attribute__((unused)))
 {
-    mknod ("/dev/sda5", S_IFBLK|0644, 8 << 8 | 5);
-    mount ("/dev/sda5", "/root", "ext4", MS_RDONLY, NULL);
-    unlink ("/dev/sda5");
-    if (chdir("/root") != 0) return -1;
-    mount(".", "/", NULL, MS_MOVE, NULL);
-    if (chroot(".") != 0) return -1;
-    if (chdir("/") != 0) return -1;
+    ls(".");
+
+    create_and_mount("/dev/sda");
+    ls("dev");
+    create_and_mount("/dev/sda1");
+    ls("dev");
+    create_and_mount("/dev/sda2");
+    ls("dev");
+    create_and_mount("/dev/sda5");
+    ls("dev");
+
+    ls(".");
+//    printf("dev\n");
+//    chdir("dev");
+    ls("dev");
+//    chdir("..");
+//    ls();
+    
+//    printf("root\n");
+//    chdir("root");
+    ls("root");
+//    chdir("..");
+//    ls();
+
+    //unlink ("/dev/sda1");
+
+    if (chdir("/root") != 0)
+        perror("chdir");
+    ls(".");
+
+    if(mount(".", "/", NULL, MS_MOVE, NULL))
+        perror("mount");
+
+    if (chroot(".") != 0)
+        perror("chroot");
+    ls(".");
+
+    if (chdir("/") != 0)
+        perror("chdir");
+    ls(".");
+
     execl("/sbin/init", "/sbin/init", NULL);
+    
 }
